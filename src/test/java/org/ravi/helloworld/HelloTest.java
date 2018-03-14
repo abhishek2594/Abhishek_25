b9097da68ec0b86ba7603b965d956f79ea1ee886
Training_M2B.01.59 <Training_M2B.01.59@accenture.com>
2018-03-14 16:58:41 +0530

    Adding project content

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -23,8 +23,8 @@ public class HelloTest {
 		driver = new FirefoxDriver();*/
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/Helloworld.war/");
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+		driver.get("http://localhost:8080/HelloWorld.war/");
+		driver.manage().timeouts().implicitlywait(1, TimeUnit.MINUTES);
 	}
 
 	@Test
